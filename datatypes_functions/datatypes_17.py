af18bf6461211137b219cb8f95af5ca655eedc2b
grgprarup <grgprarup@gmail.com>
2021-02-13 18:10:36 +0545

    datatypes question 17

---
+++ datatypes_functions/datatypes_17.py
@@ -0,0 +1,9 @@
+"""
+    17. Write a Python program to multiplies all the items in a list.
+"""
+input_list = [1,2,3,4,5,6,7,8,9,10]
+mul = 1
+print(type(mul))
+for i in input_list:
+    mul *= i
+print(mul)
