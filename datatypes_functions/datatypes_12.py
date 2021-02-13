e7915cfc2b902998682ee2742b7edf6427c7f674
grgprarup <grgprarup@gmail.com>
2021-02-13 16:10:48 +0545

    datatypes question 12

---
+++ datatypes_functions/datatypes_12.py
@@ -0,0 +1,10 @@
+"""
+    12. Write a Python script that takes input from the user and displays that input
+back in upper and lower cases.
+"""
+input_string = input('Enter a string: ')
+upper_case = input_string.upper()
+print(upper_case)
+
+lower_case = input_string.lower()
+print(lower_case)
