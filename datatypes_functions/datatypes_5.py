508b606ae67af6627e68379a41f922136d56b232
grgprarup <grgprarup@gmail.com>
2021-02-14 16:51:36 +0545

    datatypes question 5

---
+++ datatypes_functions/datatypes_5.py
@@ -0,0 +1,16 @@
+"""
+    5. Write a Python program to add 'ing' at the end of a given string (length should
+be at least 3). If the given string already ends with 'ing' then add 'ly' instead. If the
+string length of the given string is less than 3, leave it unchanged.
+Sample String : 'abc'
+Expected Result : 'abcing'
+Sample String : 'string'
+Expected Result : 'stringly'
+"""
+input_string = 'abc'
+if len(input_string) >= 3:
+    if input_string.endswith('ing'):
+        input_string += 'ly'
+    else:
+        input_string += 'ing'
+print(input_string)
