24ac03ce10c85bdb5cf851f0cfcc61efd7a93484
grgprarup <grgprarup@gmail.com>
2021-02-14 11:16:45 +0545

    datatypes question 31

---
+++ datatypes_functions/datatypes_31.py
@@ -0,0 +1,6 @@
+"""
+    31. Write a Python program to iterate over dictionaries using for loops.
+"""
+input_dict = {'first_name':'David','last_name':'Chhetri','address':'Kathmandu','mobile_no':'9846323445'}
+for item in input_dict.items():
+    print(item)
