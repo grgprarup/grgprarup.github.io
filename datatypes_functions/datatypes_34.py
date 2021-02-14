9a9ece3d0344cfc10ba6c98bcff527ba7d416f3c
grgprarup <grgprarup@gmail.com>
2021-02-14 11:20:54 +0545

    datatypes question 34

---
+++ datatypes_functions/datatypes_34.py
@@ -0,0 +1,7 @@
+"""
+    34. Write a Python script to merge two Python dictionaries.
+"""
+dict1 = {'first_name':'David','last_name':'Chhetri'}
+dict2 = {'address':'Kathmandu','mobile_no':'9846323445'}
+dict1.update(dict2)
+print(dict1)
