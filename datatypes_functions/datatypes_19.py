6f630ee906610063ac986418fd33d3e91fef91c3
grgprarup <grgprarup@gmail.com>
2021-02-14 15:56:24 +0545

    datatypes question 19

--- datatypes_functions/datatypes_19.py
+++ datatypes_functions/datatypes_19.py
@@ -1,3 +1,7 @@
 """
     19. Write a Python program to get the smallest number from a list.
 """
+input_list = [3456,35,46,87,234,45,76,8797,3434,65,444]
+input_list.sort()
+smallest = input_list[0]
+print(smallest)
