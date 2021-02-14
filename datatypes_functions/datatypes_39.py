f7954073d34634f69a359189b4fc40e678042024
grgprarup <grgprarup@gmail.com>
2021-02-14 11:30:04 +0545

    datatypes question 39

---
+++ datatypes_functions/datatypes_39.py
@@ -0,0 +1,8 @@
+"""
+    39. Write a Python program to unpack a tuple in several variables.
+"""
+input_tuple = (1,2,3,4,5,6,7,8,9)
+(a, b, c, d, e, f, g, h, i) = input_tuple
+# (a, b, c, d, e, f, g, h, i) = (1,2,3,4,5,6,7,8,9)
+
+print(a,b,c,d,e,f,g,h,i)
