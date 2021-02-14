3e9fedcd800d5d402c51d7c5b0adb9583efe20aa
grgprarup <grgprarup@gmail.com>
2021-02-14 11:41:15 +0545

    datatypes question 40

---
+++ datatypes_functions/datatypes_40.py
@@ -0,0 +1,6 @@
+"""
+    40. Write a Python program to add an item in a tuple.
+"""
+# input_tuple = ()    # creates an empty tuple
+input_tuple = (1,2,3,4) # creates an tuple with values
+# values in tuple must be inserted during creation of tuple, item cannot be added to tuple because tuple is immutable
