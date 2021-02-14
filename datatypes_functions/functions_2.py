63e2c72590578fcb2b593fbad09ad5c25f1642b1
grgprarup <grgprarup@gmail.com>
2021-02-14 23:11:02 +0545

    functions question 2

---
+++ datatypes_functions/functions_2.py
@@ -0,0 +1,10 @@
+"""
+    2. Write a Python function to sum all the numbers in a list.
+Sample List : (8, 2, 3, 0, 7)
+Expected Output : 20
+"""
+def sum_all(input_list):
+    return sum(input_list)
+
+input_list = [8, 2, 3, 0, 7]
+print(sum_all(input_list))
