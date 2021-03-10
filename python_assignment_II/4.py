df1edd87437fd57977d22e7e89ce8430062726bf
Prarup Gurung <grgprarup@gmail.com>
2021-03-10 18:39:58 +0545

    question 4 completed

---
+++ python_assignment_II/4.py
@@ -0,0 +1,14 @@
+"""
+4. Create a list. Append the names of your colleagues and friends to it.
+Has the id of the list changed? Sort the list. What is the first item on
+the list? What is the second item on the list?
+"""
+# names = []
+# or
+names = list()
+names.append('Bibek Thapa')
+names.append('John Sharma')
+names.append('Garima Maharjan')
+names.append('Pratima Shrestha')
+names.sort()
+print(f'First item: {names[0]}\nSecond Item: {names[1]}')
